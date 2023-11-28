d956269b488029d7bdf51a1c8afaa7d28050da2e
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-28 14:15:52 +0100

    Turn off passwd auth

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,21 +1,16 @@
 # File: ssh_config.pp
 include stdlib
 
-file{'ssh_config':
-   path => '/home/ubuntu/.ssh/ssh_config',
-   ensure => present
-}
-
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/home/ubuntu/.ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Delare identity file':
   ensure => present,
-  path   => '/home/ubuntu/.ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
