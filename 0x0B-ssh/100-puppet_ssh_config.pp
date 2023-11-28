03046259ca9b468adef0ed4833617f2b7ebf1ddc
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-28 14:13:47 +0100

    /home/ubuntu/.ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,21 @@
 # File: ssh_config.pp
+include stdlib
 
-file { '-/.ssh/config':
-  ensure  => present,
-  mode    => '0600',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => "# Puppet-managed SSH client configuration\n\nHost 54.208.171.118
-  IdentityFile ~/.ssh/school
-  PasswordAuthentication no",
+file{'ssh_config':
+   path => '/home/ubuntu/.ssh/ssh_config',
+   ensure => present
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
