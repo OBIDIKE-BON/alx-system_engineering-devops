85a78ab7af5eba6127ad89f6a908c9265d6b140a
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-22 14:20:30 +0100

    .pp script to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,7 @@
 # .pp script to install flask
 
-  exec { 'install_flask':
-    command => 'sudo pip3 install flask==2.1.0',
-    # Only run if the package python3-pip is installed successfully
-    require => Package['python3-pip'],
-    # Don't run if Flask is already installed and at the correct version
-    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
   }
