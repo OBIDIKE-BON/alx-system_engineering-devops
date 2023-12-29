457dbe79350797141014cfe18ec1332dbe3c4877
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-29 08:51:40 +0100

    ok

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,12 @@
 # pp script to install flask
+package {'python3':
+  ensure   => present,
+}
+
+package {'pip3':
+  ensure   => present,
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
