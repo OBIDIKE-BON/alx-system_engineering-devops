351daa0cb678ec2ca77ef5addb64a1299ebec3b7
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-02 20:53:48 +0100

    module to fetch tasks and save as csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+module to fetch tasks and save as csv
+"""
+import csv
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    tasks_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}'
+    tasks = requests.get(tasks_url).json()
+    USERNAME = requests.get(user_url).json().get("username")
+    output=[]
+    tasks_len = len(tasks)
+    for idx in range(tasks_len):
+        task = tasks[idx]
+        STATUS = task.get('completed')
+        TITLE = task.get('title')
+        output.append(f"{USER_ID},{USERNAME},{STATUS},{TITLE}".split(','))
+
+    with open(f"{USER_ID}.csv", "w")as csvFile:
+        write = csv.writer(csvFile, quoting=csv.QUOTE_MINIMAL)
+        write.writerows(output)
