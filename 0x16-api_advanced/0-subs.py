89291ce80c289856f988c37123e291add981c769
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-01 18:17:53 +0100

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,6 +14,6 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
-        return subscribers
+        return int(subscribers)
     else:
         return 0
