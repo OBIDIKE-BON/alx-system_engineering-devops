60d879a581e6506cac1da8b09f030c7efd1a294d
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-01 18:19:52 +0100

    ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,31 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return int(subscribers)
-    else:
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    # grab info about all users
+    url = base_url + '{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+
+    try:
+        # grab the info about the users' tasks
+        data = resp.get('data')
+        subscribers = data.get('subscribers')
+    except Exception as e:
         return 0
+    if subscribers is None:
+        return 0
+    return int(subscribers)
