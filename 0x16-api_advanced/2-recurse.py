4a492d3de54b59606ea4ce9e682a1bfc4d75f5f0
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-01 18:33:08 +0100

    ok

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,11 +9,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
+    params = {"after": after, "count": count, "limit": 100}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
